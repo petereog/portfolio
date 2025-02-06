7afdba3096f1cf6ee6ec07167adab061d8d75341
petereog <ogungbepeter1@gmail.com>
2025-02-06 20:51:02 +0100

    main

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div className='bg-black'>
+  <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-white">Welcome to My Home Page</h1>
+        <p className="mt-4 text-lg text-gray-600">Your content is perfectly centered!</p>
+      </div>
+    </div>
+    </div>
+  )
+}
